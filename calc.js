a99bee705d5b5bdeb6b7ec2d32fe48651fbe0c22
ayushdixit210801 <43822903+ayushdixit210801@users.noreply.github.com>
2022-06-06 18:14:48 +0530

    removed the commaAdder function

--- calc.js
+++ calc.js
@@ -5,102 +5,8 @@ const buttons = document.querySelectorAll("button");
 let expression = "";
 let store = "";
 
-function commaAdder(string) {
-  let newString = "";
-  let convertedString = `${string}`;
-  let ctr = 0;
-
-  if (convertedString === "Syntax Error") return convertedString;
-
-  if (convertedString === "Infinity") return convertedString;
-
-  if (convertedString === "undefined") return convertedString;
-
-  if (convertedString.indexOf(".") == -1) {
-    for (let i = convertedString.length - 1; i >= 0; i--) {
-      ctr++;
-      if (ctr === 4) {
-        newString = convertedString[i] + "," + newString;
-        ctr = 1;
-      } else {
-        newString = convertedString[i] + newString;
-      }
-    }
-  } else {
-    for (let i = convertedString.indexOf(".") - 1; i >= 0; i--) {
-      ctr++;
-      if (ctr === 4) {
-        newString = convertedString[i] + "," + newString;
-        ctr = 1;
-      } else {
-        newString = convertedString[i] + newString;
-      }
-    }
-    newString += convertedString.slice(convertedString.indexOf("."));
-  }
-  return newString;
-}
-
-function commify(string) {
-  let newString = "";
-  let convertedString = `${string}`;
-  let st = 0,
-    end = 0;
-
-  if (convertedString === "Syntax Error") return convertedString;
-
-  if (convertedString === "Infinity") return convertedString;
-
-  if (convertedString === "undefined") return convertedString;
-
-  for (let i = 0; i < convertedString.length; i++) {
-    if (
-      convertedString[i] === "×" ||
-      convertedString[i] === " " ||
-      convertedString[i] === "-" ||
-      convertedString[i] === "+" ||
-      convertedString[i] === "÷" ||
-      convertedString[i] === "%" ||
-      i === convertedString.length - 1
-    ) {
-      if (
-        i === convertedString.length - 1 &&
-        (convertedString[i] === "×" ||
-          convertedString[i] === " " ||
-          convertedString[i] === "-" ||
-          convertedString[i] === "+" ||
-          convertedString[i] === "÷" ||
-          convertedString[i] === "%")
-      ) {
-        newString +=
-          commaAdder(convertedString.slice(st, i)) + convertedString[i];
-      } else if (
-        i === convertedString.length - 1 &&
-        !(
-          convertedString[i] === "×" ||
-          convertedString[i] === " " ||
-          convertedString[i] === "-" ||
-          convertedString[i] === "+" ||
-          convertedString[i] === "÷" ||
-          convertedString[i] === "%"
-        )
-      ) {
-        newString += commaAdder(convertedString.slice(st, i + 1));
-      } else {
-        end = i;
-        newString +=
-          commaAdder(convertedString.slice(st, end)) + convertedString[i];
-        st = i + 1;
-      }
-    }
-  }
-  return newString;
-}
-
 function calculate(exp) {
   try {
-    console.log(exp);
-    console.log(exp.indexOf("//") != -1 || exp.indexOf("**") != -1);
     if (exp.indexOf("//") != -1 || exp.indexOf("**") != -1) {
       display.value = "Syntax Error";
       currentAnswer.value = "Syntax Error";
@@ -125,7 +31,7 @@ buttons.forEach((button) => {
     if (button.id === "=") {
       display.value = currentAnswer.value;
       currentAnswer.value = "";
-      expression = display.value.replace(",", "").trim();
+      expression = display.value.trim();
       store = expression;
       if (display.value === "undefined") {
         display.value = "";
@@ -147,15 +53,15 @@ buttons.forEach((button) => {
       if (isNaN(display.value[display.value.length - 2])) {
         store = store.slice(0, store.length - 1);
         expression = expression.slice(0, expression.length - 1);
-        display.value = commify(store.replace(",", "").trim());
+        display.value = store.trim();
         currentAnswer.value = "";
       } else {
         store = store.slice(0, store.length - 1);
         expression = expression.slice(0, expression.length - 1);
-        display.value = commify(store.replace(",", "").trim());
+        display.value = store.trim();
         temp = expression;
         try {
-          if (isNaN(temp)) currentAnswer.value = commify(calculate(temp));
+          if (isNaN(temp)) currentAnswer.value = calculate(temp);
         } catch (err) {
           currentAnswer.value = "";
         }
@@ -170,9 +76,9 @@ buttons.forEach((button) => {
         button.id != "/" &&
         button.id != "%"
       )
-        if (isNaN(temp)) currentAnswer.value = commify(calculate(temp));
+        if (isNaN(temp)) currentAnswer.value = calculate(temp);
       store += button.innerText;
-      display.value = commify(store.replace(",", "").trim());
+      display.value = store.trim();
     }
   });
 });
